1f7e4490a7409ebd7185dc3e90006178ab9fc599
voie420 <mvoiej@outlook.com>
2025-05-27 00:14:24 +0200

    Adding the providers to the root layout file.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,9 @@ import './globals.css';
 import type { Metadata } from 'next';
 
 import { Inter } from 'next/font/google';
+import Providers from './providers';
 
-import Header from './components/header';
+// import Header from './components/header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -30,6 +31,8 @@ export default function RootLayout({
         {modals}
 
         {children}
+
+        <Providers />
       </body>
     </html>
   );
