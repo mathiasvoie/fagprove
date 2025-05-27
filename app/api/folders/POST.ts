3baeff4d2c9ed7b48fbec50f8e1c5f5b2b26b57d
voie420 <mvoiej@outlook.com>
2025-05-27 13:28:57 +0200

    Making the API endpoint work with image uploading for a proof of concept which will be used throughout the project.

--- app/api/folders/POST.ts
+++ app/api/folders/POST.ts
@@ -3,13 +3,16 @@ import { NextResponse } from 'next/server';
 import { prisma } from '@/app/lib/prisma';
 import { CreateFolderSchema } from '@/app/schemas/CreateFolderSchema';
 import { revalidatePath } from 'next/cache';
+import { ImageService } from '@/app/services/image';
 
 export async function POST(req: Request) {
-  const data = await req.json();
+  const formdata = await req.formData();
 
-  const { name, description } = data;
+  const name = formdata.get('name') as string;
+  const description = formdata.get('description') as string | undefined;
+  const image = formdata.get('image') as File | null;
 
-  const isFormValid = CreateFolderSchema.safeParse({ ...data });
+  const isFormValid = CreateFolderSchema.safeParse({ name, description });
 
   if (!isFormValid.success) {
     return NextResponse.json('Form validation failed.', { status: 401 });
@@ -25,10 +28,24 @@ export async function POST(req: Request) {
     return NextResponse.json('Folder name already exists.', { status: 400 });
   }
 
+  let imageId: string | undefined;
+  if (image) {
+    const response = await ImageService.saveImage(image);
+
+    if (!response) {
+      return NextResponse.json('Failed to save the image that was provided.', {
+        status: 500,
+      });
+    }
+
+    imageId = response;
+  }
+
   await prisma.folders.create({
     data: {
       name,
       description,
+      imageId: imageId,
     },
   });
 
