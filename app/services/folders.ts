6e55c16dc1b80eedccd47b46d7790dd5ddb7b8b6
voie420 <mathiasvj@outlook.com>
2025-05-28 19:06:19 +0200

    Creating Folders class for ease of access to folders.

--- app/services/folders.ts
+++ app/services/folders.ts
@@ -1,9 +1,78 @@
 import { prisma } from '../lib/prisma';
 
-export async function getFolders() {
-  return await prisma.folders.findMany({
-    include: {
-      image: true,
-    },
-  });
+export class Folders {
+  public static async getAll() {
+    const response = await prisma.folders.findMany({
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        image: true,
+        tools: true,
+      },
+    });
+
+    if (!response) return [];
+
+    return response.map((folder) => ({
+      id: folder.id,
+      name: folder.name,
+      description: folder.description,
+      image: folder.image,
+      tools: folder.tools.length,
+    }));
+  }
+  public static async getByUid(uid: string) {
+    return await prisma.folders.findUnique({
+      where: {
+        id: uid,
+      },
+      include: {
+        image: true,
+      },
+    });
+  }
+  public static async getNameByUid(uid: string) {
+    return (
+      await prisma.folders.findUnique({
+        where: {
+          id: uid,
+        },
+        select: {
+          name: true,
+        },
+      })
+    )?.name;
+  }
+  public static async getMetadataByUid(uid: string) {
+    const folder = await prisma.folders.findUnique({
+      where: {
+        id: uid,
+      },
+      select: {
+        name: true,
+        description: true,
+        image: true,
+        tools: {
+          select: { id: true },
+        },
+      },
+    });
+
+    if (!folder) return null;
+
+    return {
+      name: folder.name,
+      description: folder.description,
+      image: folder.image,
+      tools: folder.tools.length,
+    };
+  }
+  public static async getToolsFromUid(uid: string) {
+    return await prisma.tools.findMany({
+      where: {
+        folderId: uid,
+      },
+    });
+  }
 }
