23881abbe826fd3f51e3665d2afd10803855d22f
voie420 <mathiasvj@outlook.com>
2025-05-29 15:58:49 +0200

    Router class was a proof of concept that failed. Failed since useRouter and useSearchParams violated hook laws when in class.

--- app/services/router.ts
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { useRouter, useSearchParams } from 'next/navigation';
-
-export class RouterService {
-  private static router = useRouter();
-  private static searchParams = useSearchParams();
-}
