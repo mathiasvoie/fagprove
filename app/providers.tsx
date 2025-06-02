388d70500862768255d2e96fa977e305b110ebe0
voie420 <mathiasvj@outlook.com>
2025-06-02 12:08:39 +0200

    useQuery provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,18 +1,20 @@
 'use client';
 
 import { ToastProvider } from '@heroui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
 
 interface ProvidersProps {
   children: React.ReactNode[];
 }
 
+const queryClient = new QueryClient();
+
 export default function Providers({ children }: ProvidersProps) {
   return (
     <SessionProvider>
       <ToastProvider />
-
-      {children}
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </SessionProvider>
   );
 }
